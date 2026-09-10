b19202d28e6cd5c054d8b78aaac3f6449980dee8
jvo-james <jadelstech@gmail.com>
2026-09-10 07:19:35 -0700

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ window.BF_CONFIG = {
     appId: '1:720382521656:web:4d28d1fa6ea960690451cd'
   },
   contactEmail: 'bandfactoryy@gmail.com',
-  paystackPublicKey: 'pk_live_a2128a5ce9b17b510541dfa62415ed3d5a649c18',
+  paystackPublicKey: 'pk_test_288adc78cd4451e3510f5ed9ee3ca794a29536fc',
   secureOrderEndpoint: '/.netlify/functions/verify-payment',
   mapboxAccessToken: 'pk.eyJ1IjoianZvamFtZXMiLCJhIjoiY21zdHlsMnhiMGJ4eDJ5c2ZmZWFwcG55biJ9.keYOyrSD7yFhj39sTHBOQw',
   pickup: {
